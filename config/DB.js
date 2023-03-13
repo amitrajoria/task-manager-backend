c808d027a3672f2274973fa025d2e85b5eb98176
amitrajoria <amitrajoria.it@gmail.com>
2023-03-13 16:33:56 +0530

    updated DB URL

--- config/DB.js
+++ config/DB.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require('dotenv').config()
 
-const connection = mongoose.connect(process.env.DB_URL);
+const connection = mongoose.connect("mongodb+srv://amitrajoria:amitrajoria@amitcluster.x1l0ccx.mongodb.net/task_manager?retryWrites=true&w=majority");
 
 module.exports = {
     connection
